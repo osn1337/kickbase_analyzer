d3532f280a57fa831bae5b3673a5f128e87da8cb
osn1337 <t.ossendorf@hotmail.com>
2026-09-10 08:46:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "41": { full: "VfL Bochum", short: "BOC" },
         "42": { full: "Holstein Kiel", short: "KIE" },
         "43": { full: "RB Leipzig", short: "RBL" },
-        "50": { full: "1. FC Heidenheim", short: "FCH" }
+        "50": { full: "1. FC Heidenheim", short: "FCH" },
         "77": { full: "SV Elversberg", short: "ELV" }        
     };
 
